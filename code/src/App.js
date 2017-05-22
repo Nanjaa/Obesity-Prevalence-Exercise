99d8c8b3a8a509d3404e74f853203218e7918964
Nanja <steph.o-91@hotmail.com>
2017-05-21 21:41:34 -0700

    Created basic React application for the exercise

---
+++ code/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Obesity Prevalence Exercise</h1>
+          <a href="https://github.com/Nanjaa/Obesity-Prevalence-Exercise" target="_blank">Github</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

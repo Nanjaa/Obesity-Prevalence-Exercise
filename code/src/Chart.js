00a769a802911ef3480e2065a405267daaaaf7ed
Nanja <steph.o-91@hotmail.com>
2017-05-23 21:06:36 -0700

    Added axis to the graph

--- code/src/Chart.js
+++ code/src/Chart.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import d3 from 'd3';
-import ScatterPlot from './ScatterPlot.js';
 
 class Chart extends React.Component {
 
@@ -17,6 +16,7 @@ class Chart extends React.Component {
         };
     }
 
+
     xMax(data) {
         var d3 = require('d3');
         return d3.max(data, (d) => d[0]);
@@ -26,27 +26,74 @@ class Chart extends React.Component {
         return d3.max(data, (d) => d[1]);
     }
 
-    renderCircles() {
+    xScale(num, needFunc) {
         var d3 = require('d3');
-
-        var yScale = d3.scaleLinear()
-                .domain([0, this.yMax(this.state.data)])
-                .range([this.state.height - this.state.padding, this.state.padding]);
-
-        var xScale = d3.scaleLinear()
+        var xNum = d3.scaleLinear()
                 .domain([0, this.xMax(this.state.data)])
                 .range([this.state.padding, this.state.width - this.state.padding * 2]);
+        if(needFunc) {
+            return xNum;
+        }
+        else {
+            return xNum(num);
+        }
+    }
+    yScale(num, needFunc) {
+        var d3 = require('d3');
+        var yNum = d3.scaleLinear()
+                .domain([0, this.yMax(this.state.data)])
+                .range([this.state.height - this.state.padding, this.state.padding]);
+        if(needFunc) {
+            return yNum;
+        }
+        else {
+            return yNum(num)
+        }
+    }
 
+
+    renderCircles() {
         return (coords, index) => {
             const circleProps = {
-                cx: xScale(coords[0]),
-                cy: yScale(coords[1]),
+                cx: this.xScale(coords[0]),
+                cy: this.yScale(coords[1]),
                 r: 2,
                 key: index
             };
             return <circle {...circleProps}/>;
         };
     };
+
+    renderAxis(orient, scale) {
+        var d3 = require('d3');
+
+        var xNode = this.refs.xAxis;
+        var xAxis = d3.axisBottom()
+                .scale(this.xScale(0, true));
+
+        var yNode = this.refs.yAxis;
+        var yAxis = d3.axisLeft()
+                .scale(this.yScale(0, true));
+
+        d3.select(xNode).call(xAxis);
+        d3.select(yNode).call(yAxis);
+    }
+
+    translateAxis(xy) {
+        if(xy == 'x') {
+            var translate = `translate(0, ${this.state.height - this.state.padding})`;
+            return translate;
+        }
+        else {
+            var translate = `translate(${this.state.padding}, 0)`;
+            return translate;
+        }
+        
+    }
+
+    componentDidMount() {
+        this.renderAxis();
+    }
 
     render(props) {
         return (
@@ -56,6 +103,10 @@ class Chart extends React.Component {
                     <g>
                         {this.state.data.map(this.renderCircles())}
                     </g>
+
+                    <g className="axis" ref="xAxis" transform={this.translateAxis('x')}></g>
+                    <g className="axis" ref="yAxis" transform={this.translateAxis('y')}></g>
+                    
                 </svg>
             </div>
         );
